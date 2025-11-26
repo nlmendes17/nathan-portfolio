fb93051bdb4bbb2dac3caabcbb9345bf302f145c
nlmendes17 <nathanlivemendes22@gmail.com>
2025-11-26 13:33:26 -0300

    fix: tailwind carregando corretamente

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
@@ -15,4 +14,6 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
+
